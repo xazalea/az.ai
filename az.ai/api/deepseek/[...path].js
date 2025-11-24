57a9064350b577bc310ce69d3bea172a44ffeaba
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-11-23 22:15:50 -0500

    Cleanup artifacts

--- az.ai/api/deepseek/[...path].js
+++
@@ -1,7 +0,0 @@
-const app = require('../../packages/deepseek-free-api/dist/index.js');
-
-module.exports = (req, res) => {
-    const handler = app.default || app;
-    return handler(req, res);
-};
-
